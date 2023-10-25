6a1845cf7253c30cc31c46b48d3b1f518a52143a
Dorian <507644@student.fontys.nl>
2023-10-25 18:07:05 +0200

    initialized the timeslots for doctors

--- src/main/java/com/medicalcare/medicalcareappointments/configuration/db/DatabaseDataInitializer.java
+++ src/main/java/com/medicalcare/medicalcareappointments/configuration/db/DatabaseDataInitializer.java
@@ -1,7 +1,9 @@
 package com.medicalcare.medicalcareappointments.configuration.db;
 
+import com.medicalcare.medicalcareappointments.business.impl.appointment.ReverseTimeSlotConverter;
 import com.medicalcare.medicalcareappointments.domain.account.AccountType;
 import com.medicalcare.medicalcareappointments.domain.appointment.AppointmentStatus;
+import com.medicalcare.medicalcareappointments.domain.appointment.TimeSlot;
 import com.medicalcare.medicalcareappointments.persistence.AccountRepository;
 import com.medicalcare.medicalcareappointments.persistence.AppointmentRepository;
 import com.medicalcare.medicalcareappointments.persistence.ReviewRepository;
@@ -14,7 +16,9 @@ import org.springframework.stereotype.Component;
 import java.time.LocalDate;
 import java.time.ZoneId;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 
 @Component
 @AllArgsConstructor
@@ -26,12 +30,33 @@ public class DatabaseDataInitializer {
 
     @EventListener(ApplicationReadyEvent.class)
     public void populateWithDummyData() {
-        accountRepository.save(DoctorEntity.builder().photo("doctor").name("Emily").fname("Johnson").description("Dr. Emily Johnson is a board-certified Cardiologist with a passion for preventive cardiology. She is known for her pioneering research in heart disease prevention and her dedication to educating her patients about heart-healthy lifestyles. With over two decades of experience, Dr. Johnson has helped countless patients regain their heart health and lead fulfilling lives.").username("doctor").accountType(AccountType.Doctor).availableTimeSlots(new ArrayList<>()).Email("doctor@gmail.com").password("12345").specialization("Cardiologist").build());
-        accountRepository.save(DoctorEntity.builder().photo("doctor1").name("Sarah").fname("Anderson").description("Dr. Sarah Anderson is a renowned Gynecologist specializing in reproductive health for women of all ages. With a warm and empathetic approach, she has guided women through the various stages of life, from adolescence to pregnancy and beyond. Dr. Anderson believes in the importance of women's health and strives to empower her patients with knowledge and personalized care.").username("doctor").accountType(AccountType.Doctor).availableTimeSlots(new ArrayList<>()).Email("doctor@gmail.com").password("12345").specialization("Gynecologist").build());
-        accountRepository.save(DoctorEntity.builder().photo("doctor2").name("Michael").fname("Smith").description("Dr. Michael Smith is a leading Dermatologist known for his expertise in advanced skin care and cosmetic dermatology. He combines art and science to help his patients achieve their best skin. With a keen eye for aesthetics, Dr. Smith has transformed the lives of many, boosting their confidence through healthy and radiant skin.").username("doctor").accountType(AccountType.Doctor).availableTimeSlots(new ArrayList<>()).Email("doctor@gmail.com").password("12345").specialization("Dermatologist").build());
-        accountRepository.save(DoctorEntity.builder().photo("doctor3").name("David").fname("Patel").description("Dr. David Patel is a compassionate Cardiologist committed to improving heart health in the community. He has a strong background in interventional cardiology and has performed life-saving procedures for many patients. Dr. Patel's holistic approach to patient care includes not only treating the heart but also addressing overall wellness.").username("doctor").accountType(AccountType.Doctor).availableTimeSlots(new ArrayList<>()).Email("doctor@gmail.com").password("12345").specialization("Cardiologist").build());
+        List<TimeSlot> timeSlots = new ArrayList<>();
+
+        for (int day = Calendar.MONDAY; day <= Calendar.FRIDAY; day++) {
+            Calendar calendar = Calendar.getInstance();
+            calendar.set(Calendar.DAY_OF_WEEK, day);
+
+            for (int hour = 9; hour <= 17; hour++) {
+                calendar.set(Calendar.HOUR_OF_DAY, hour);
+                calendar.set(Calendar.MINUTE, 0);
+                calendar.set(Calendar.SECOND, 0);
+                calendar.set(Calendar.MILLISECOND, 0);
+                Date startTime = calendar.getTime();
+
+                calendar.add(Calendar.HOUR, 1);
+                Date endTime = calendar.getTime();
+
+                TimeSlot timeSlot = new TimeSlot(startTime, endTime);
+                timeSlots.add(timeSlot);
+            }
+        }
+
+        accountRepository.save(DoctorEntity.builder().photo("doctor").name("Emily").fname("Johnson").description("Dr. Emily Johnson is a board-certified Cardiologist with a passion for preventive cardiology. She is known for her pioneering research in heart disease prevention and her dedication to educating her patients about heart-healthy lifestyles. With over two decades of experience, Dr. Johnson has helped countless patients regain their heart health and lead fulfilling lives.").username("doctor").accountType(AccountType.Doctor).availableTimeSlots(timeSlots.stream().map(ReverseTimeSlotConverter::convert).toList()).Email("doctor@gmail.com").password("12345").specialization("Cardiologist").build());
+        accountRepository.save(DoctorEntity.builder().photo("doctor1").name("Sarah").fname("Anderson").description("Dr. Sarah Anderson is a renowned Gynecologist specializing in reproductive health for women of all ages. With a warm and empathetic approach, she has guided women through the various stages of life, from adolescence to pregnancy and beyond. Dr. Anderson believes in the importance of women's health and strives to empower her patients with knowledge and personalized care.").username("doctor").accountType(AccountType.Doctor).availableTimeSlots(timeSlots.stream().map(ReverseTimeSlotConverter::convert).toList()).Email("doctor@gmail.com").password("12345").specialization("Gynecologist").build());
+        accountRepository.save(DoctorEntity.builder().photo("doctor2").name("Michael").fname("Smith").description("Dr. Michael Smith is a leading Dermatologist known for his expertise in advanced skin care and cosmetic dermatology. He combines art and science to help his patients achieve their best skin. With a keen eye for aesthetics, Dr. Smith has transformed the lives of many, boosting their confidence through healthy and radiant skin.").username("doctor").accountType(AccountType.Doctor).availableTimeSlots(timeSlots.stream().map(ReverseTimeSlotConverter::convert).toList()).Email("doctor@gmail.com").password("12345").specialization("Dermatologist").build());
+        accountRepository.save(DoctorEntity.builder().photo("doctor3").name("David").fname("Patel").description("Dr. David Patel is a compassionate Cardiologist committed to improving heart health in the community. He has a strong background in interventional cardiology and has performed life-saving procedures for many patients. Dr. Patel's holistic approach to patient care includes not only treating the heart but also addressing overall wellness.").username("doctor").accountType(AccountType.Doctor).availableTimeSlots(timeSlots.stream().map(ReverseTimeSlotConverter::convert).toList()).Email("doctor@gmail.com").password("12345").specialization("Cardiologist").build());
         accountRepository.save(DoctorEntity.builder().photo("doctor4").name("Michael").fname("Johnson").description("Dr. Michael Johnson is a dedicated Gynecologist who believes in the importance of women's reproductive health. He has a reputation for providing comprehensive care to women at every stage of life. Dr. Johnson's approach is marked by empathy and a commitment to improving the well-being of his patients.").username("doctor").accountType(AccountType.Doctor).availableTimeSlots(new ArrayList<>()).Email("doctor@gmail.com").password("12345").specialization("Gynecologist").build());
-        accountRepository.save(DoctorEntity.builder().photo("doctor5").name("Emily").fname("Anderson").description("Dr. Emily Anderson is a leading Dermatologist who blends science and art in her practice. She is known for her innovative skincare solutions and her personalized approach to each patient's unique needs. Dr. Anderson's goal is to help her patients achieve not only healthy skin but also a renewed sense of confidence and well-being.").username("doctor").accountType(AccountType.Doctor).availableTimeSlots(new ArrayList<>()).Email("doctor@gmail.com").password("12345").specialization("Dermatologist").build());
+        accountRepository.save(DoctorEntity.builder().photo("doctor5").name("Emily").fname("Anderson").description("Dr. Emily Anderson is a leading Dermatologist who blends science and art in her practice. She is known for her innovative skincare solutions and her personalized approach to each patient's unique needs. Dr. Anderson's goal is to help her patients achieve not only healthy skin but also a renewed sense of confidence and well-being.").username("doctor").accountType(AccountType.Doctor).availableTimeSlots(timeSlots.stream().map(ReverseTimeSlotConverter::convert).toList()).Email("doctor@gmail.com").password("12345").specialization("Dermatologist").build());
         accountRepository.save(UserEntity.builder().username("user").accountType(AccountType.User).Email("user@gmail.com").password("12345").firstName("Vieru").lastName("Dorian").dateOfBirth(new Date()).build());
         accountRepository.save(AdminEntity.builder().username("admin").accountType(AccountType.Admin).Email("admin@gmail.com").password("12345").position("boss").build());
 
