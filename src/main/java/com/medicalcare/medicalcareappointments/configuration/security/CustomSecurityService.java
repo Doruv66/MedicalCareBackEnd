248f61b7df005def454ffbc6ec7b15137853dcd1
Dorian Vieru <d.vieru@student.fontys.nl>
2024-01-08 12:12:04 +0100

    removed 2 bug smells

--- src/main/java/com/medicalcare/medicalcareappointments/configuration/security/CustomSecurityService.java
+++ src/main/java/com/medicalcare/medicalcareappointments/configuration/security/CustomSecurityService.java
@@ -1,8 +1,6 @@
 package com.medicalcare.medicalcareappointments.configuration.security;
 
 import com.medicalcare.medicalcareappointments.configuration.security.token.AccessToken;
-import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
@@ -14,7 +12,7 @@ public class CustomSecurityService {
     public CustomSecurityService(AccessToken accessToken) {
         this.accessToken = accessToken;
     }
-    
+
     public boolean isAccountIdMatching(Long accId, Authentication authentication) {
 
         //check for null
