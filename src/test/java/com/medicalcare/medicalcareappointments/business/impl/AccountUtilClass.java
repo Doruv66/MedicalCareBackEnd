31112ba4a05c903de9e1884052f7abcabb7f1ce6
Dorian Vieru <d.vieru@student.fontys.nl>
2024-01-18 11:25:07 +0100

    pipeline test

--- src/test/java/com/medicalcare/medicalcareappointments/business/impl/AccountUtilClass.java
+++ src/test/java/com/medicalcare/medicalcareappointments/business/impl/AccountUtilClass.java
@@ -11,6 +11,7 @@ import java.util.ArrayList;
 import java.util.Date;
 
 public class AccountUtilClass {
+    
     public static Patient createPatient() {
         return Patient.builder()
                 .accountId(5L)
